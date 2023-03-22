4cec43e0f1cb0e62eff4ca189d5813a46d7dd789
Nuno Lemos <nuno@v-a.pt>
2023-03-22 16:08:44 +0000

    Config Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
-  slugify: {
-    enabled: true,
-    config: {
-      contentTypes: {
-        article: {
-          field: "slug",
-          references: "title",
+      upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            delete: {},
+          },
         },
       },
-    },
-  },
-});
+    });
