2c6bc3bfb1964199f04a05f96c6e4c13855fa221
Nuno Lemos <nuno@v-a.pt>
2023-05-15 09:13:06 +0100

    Remove Cloudinary and add new Content Types

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,13 @@
 module.exports = ({ env }) => ({
-      upload: {
-        config: {
-          provider: 'cloudinary',
-          providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-          },
-          actionOptions: {
-            upload: {},
-            delete: {},
-          },
+  slugify: {
+    enabled: true,
+      config: {
+        contentTypes: {
+        article: {
+          field: "slug",
+          references: "title",
         },
       },
-    });
+    },
+  },
+});
