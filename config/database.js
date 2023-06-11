e491e88f02dd629c1ab2ad8e8e1f10f1cd61bef8
Nuno Lemos <nuno@v-a.pt>
2023-06-11 14:07:22 +0100

    Change database config to postgres

--- config/database.js
+++ config/database.js
@@ -4,13 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
+      host: env("DATABASE_HOST", '0.0.0.0'),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      // ssl: env.bool('DATABASE_SSL', true),
-      "ssl": { "rejectUnauthorized": false }
+      ssl: false 
     },
   },
 });
