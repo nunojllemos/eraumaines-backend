7496877c48c09e67b8e96e82f67b6aa3c804fc63
Nuno Lemos <nuno@v-a.pt>
2023-06-11 14:42:16 +0100

    Change database config schema

--- config/database.js
+++ config/database.js
@@ -9,6 +9,7 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA", "public"),
       ssl: false 
     },
   },
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,6 +7,7 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA", "public"),
       // ssl: env.bool('DATABASE_SSL', true),
       "ssl": { "rejectUnauthorized": false }
     },
