0add9f4129cc1b7795651dd57d5adb11464e2855
nunojllemos <nunojllemos@gmail.com>
2025-02-16 21:22:29 +0000

    Fix homepage single type component

--- src/api/homepage/services/homepage.js
+++ src/api/homepage/services/homepage.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * home service
+ * homepage service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::home.home');
+module.exports = createCoreService('api::homepage.homepage');
